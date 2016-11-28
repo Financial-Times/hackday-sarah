80656e7efd58999f358ae60c2882a6da80901dec
Luke Blaney <luke.blaney@ft.com>
2016-11-28 14:50:25 +0000

    add more fields to model

--- model.go
+++ model.go
@@ -6,9 +6,15 @@ type content struct {
 	Standfirst        string `json:"standfirst"`
 	Byline            string `json:"byline"`
 	PublishedDate     string `json:"publishedDate"`
+	ImageUrl          string `json:"image"`
+	Tags           [] tag    `json:"tags"`
 }
 type organisation struct {
 	Title string `json:"title"`
 	Description string `json:"description"`
 	Stories [] content `json:"stories"`
+}
+type tag struct {
+	URL    string `json:"url"`
+	Label  string `json:"label"`
 }
